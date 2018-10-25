7762f923197d46cc3322733a2ef0ee3e01343971
Nathan WILCKE <SESA474351@schneider-electric.com>
2018-10-25 21:59:50 +0200

    Somes cool changes

--- Booking v2/Classes/Util.cs
+++ Booking v2/Classes/Util.cs
@@ -8,6 +8,15 @@ namespace Booking_v2.Classes
 {
     static class Util
     {
+        /// <summary>
+        /// Gets the combo id.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <returns></returns>
+        /// =====================================================================================
+        /// Modification : Initial : 25/10/2018 |N.Wilcké (SESA474351)
+        ///                          XX/XX/XXXX | X.XXX (SESAXXXXX)      
+        /// =====================================================================================
         public static int GetComboId(string str)
         {
             int id = Int32.Parse(str.Split('-')[0].Trim());
